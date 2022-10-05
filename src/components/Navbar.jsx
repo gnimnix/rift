d9fc35a0e22e0f624a9c67395d026b76c685c3e2
Lu Xinming <lu_xinming@rvhs.edu.sg>
2022-10-05 15:17:17 +0800

    added home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { Link } from "gatsby"
+
+
+class Navlink extends React.Component {
+    render() {
+        return (
+            <Link to={this.props.link} className={this.props.className}> {this.props.text} </Link>
+        )
+    }
+}
+
+
+export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.links = [
+            {
+                text: 'Home',
+                link: '/',
+                className: "nav-link"
+            }
+        ]
+    }
+    render() {
+        return (
+            <>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <ul className="navbar-nav">
+                        {this.links.map(link => (
+                        <li className="nav-item">
+                            <Navlink {...link}/>
+                        </li>
+                        ))}
+                        </ul>
+                    </div>
+                </nav>
+            </>
+        )
+    }
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+
+
+export default function IndexPage() {
+    return (
+        <>
+            <Navbar/>
+            <h1>Hello World</h1>
+        </>
+    )
+}

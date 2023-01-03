52c83012987904aaf4938bd307d135972983e614
Lu Xinming <lu_xinming@rvhs.edu.sg>
2023-01-03 19:02:08 +0800

    Finished updating the index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,122 +1,60 @@
-import React, {useState} from "react"
+import React, {useEffect, useRef, useState} from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
-import "../css/index.css"
-
-
-function IndexPageCarousel() {
-    const carouselStyle = {
-        height: "80vh"
-    }
-    return (
-        <>
-            <div id="indexPageCarousel" 
-            className="carousel slide d-flex align-items-center" 
-            data-bs-ride="carousel"
-            style={carouselStyle}>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <div className="d-flex h-100 align-items-center justify-content-center">
-                            <p 
-                            className="text-uppercase fs-1"
-                            style={{
-                                    fontFamily: "Open Sans",
-                                }
-                            }
-                            >Hello</p>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="d-flex h-100 align-items-center justify-content-center">
-                            <p 
-                            className="text-uppercase fs-1"
-                            style={{
-                                    fontFamily: "Roboto Mono",
-                                }}
-                            >Welcome</p>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="d-flex h-100 align-items-center justify-content-center">
-                            <p 
-                            className="text-uppercase fs-1"
-                            style={{
-                                    fontFamily: "Cedarville Cursive",
-                                }}
-                            >Good Day</p>
-                        </div>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#indexPageCarousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#indexPageCarousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </>
-    )
-}
+import "../css/index.scss"
 
 
 function IntroPanel() {
-    function Card(props) {
-        return (
-            <>
-                <div className="card border bg-light p-3">
-                    <div className="card-body">
-                    {
-                        props.title && 
-                        <h3 className="card-title">{props.title}</h3>
-                    }
-                    <div className="card-text">
-                        {props.text}
-                    </div>
-                    </div>
-                </div>
-            </>
-        )
-    }
-    function firstCard (props) {
-        return (
-            <>
-                <div className="card border bg-light p-3" style={props.style}>
-                    <div className="card-body">
-                        <h3 className="card-title">Hi there,</h3>
-                        <p className="card-text">Welcome to my website which I use to blog about tech stuff, particularly about PC components.</p>
-                        <p>I am also interested in competitive programming and utilize this platform to share what I have learnt</p>
-                    </div>
-                </div>
-            </>
-        )
+    const secondWrapperRef = useRef(null)
+    const [secondWrapperVisible, setSecondWrapperVisible] = useState(false);
+
+    const callbackFunction = (entries) => {
+        const [entry] = entries
+        if (entry.isIntersecting) {
+            setSecondWrapperVisible(entry.isIntersecting)
+            observer.unobserve(secondWrapperRef.current)
+            observer.disconnect()
+        }
     }
 
-    function secondCard(props) {
-        return (
-            <>
-                <div className="card border bg-light p-3" style={{...props.style, left: "-4.5%", top: "8%"}}>
-                    <div className="card-body">
-                        <p className="card-text">This website was made using the Gatsby Framework together with ReactJS and React-Spring</p>
-                    </div>
-                </div>
-            </>
-        )
-    }
+    const observer = new IntersectionObserver(callbackFunction, {
+        threshold: 1,
+    })
 
-    const cards = [firstCard, secondCard]
-    const cardsZIndex = [useState(3), useState(0)]
+    useEffect(() => {
+        if (secondWrapperRef.current) observer.observe(secondWrapperRef.current)
+    })
 
     return (
-        <>
-        <div className="panel">
-            {cards.map((Card, i) => (
-                <Card key={i} style={{zIndex: cardsZIndex[i][0]}}/>
-            ))}
+        <div className={"index-main"}>
+            <div className={"firstIntro"}>
+                <svg viewBox={"0 -20 52.234375 30.33333396911621"} xmlns={"http://www.w3.org/2000/svg"}>
+                    <text style={{fontFamily: "Cedarville Cursive"}} >HELLO</text>
+                </svg>
+                <p>
+                    Welcome to my site. I use this website as a portfolio to showcase my programming skills and
+                    hobbies. Occasionally, I will blog about PC components and peripherals.
+                </p>
+            </div>
+            <div className={"secondIntro"}>
+                <div className={"secondIntro-wrapper"} ref={secondWrapperRef}>
+                    <h1 id={secondWrapperVisible ? "secondIntro-yellow-bg-animation" : null}>
+                        Pardon the ugly design
+                    </h1>
+                    <p id={secondWrapperVisible ? "secondIntro-blue-bg-animation": null}>
+                        Forgot to put skill points into designing &gt;.&lt;
+                    </p>
+                </div>
+            </div>
+            <div className={"thirdIntro"}>
+                <p>
+                    This website was made using GatsbyJs with vanilla CSS and hosted on a home server. It is constantly being updated and
+                    new features will be integrated to this site. I plan to migrate this site to Amazon Web Services (AWS)
+                    in the future for better performance and security.
+                </p>
+            </div>
         </div>
-        </>
     )
 }
 
@@ -125,9 +63,20 @@ export default function IndexPage() {
     return (
         <>
             <Navbar/>
-            <IndexPageCarousel/>
             <IntroPanel/>
             <Footer/>
+        </>
+    )
+}
+
+
+export const Head = () => {
+    return (
+        <>
+            <title>Welcome</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <script src="https://kit.fontawesome.com/046210d477.js" crossOrigin="anonymous"></script>
+            <link rel={"icon"} href="../images/icon.png"/>
         </>
     )
 }
